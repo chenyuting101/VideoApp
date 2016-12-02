57f48304b5598254bdc7bd8ce3101bb1cedf6e07
chenyuting <chenyuting101@outlook.com>
2016-12-02 11:48:57 +0800

    Add class "Video"

---
+++ app/src/main/java/com/cs/group/com/cs/group/entity/Video.java
@@ -0,0 +1,35 @@
+package com.cs.group.com.cs.group.entity;
+
+/**
+ * Created by chenyuting on 12/2/16.
+ */
+
+public class Video {
+    private String imageUri;
+    private String videoUri;
+    private String id;
+
+    public String getImageUri() {
+        return imageUri;
+    }
+
+    public void setImageUri(String imageUri) {
+        this.imageUri = imageUri;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getVideoUri() {
+        return videoUri;
+    }
+
+    public void setVideoUri(String videoUri) {
+        this.videoUri = videoUri;
+    }
+}
