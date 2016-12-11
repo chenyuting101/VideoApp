e9adda4fbf6a17a6fc5e6f9df44561fb7bee69fc
chenyuting <chenyuting101@outlook.com>
2016-12-11 19:31:58 +0800

    Update class "Videoe"

--- app/src/main/java/com/cs/group/com/cs/group/entity/Video.java
+++ app/src/main/java/com/cs/group/com/cs/group/entity/Video.java
@@ -9,6 +9,16 @@ public class Video {
     private String videoUri;
     private String id;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    private String username;
+
     public String getImageUri() {
         return imageUri;
     }
@@ -32,4 +42,5 @@ public class Video {
     public void setVideoUri(String videoUri) {
         this.videoUri = videoUri;
     }
+
 }
